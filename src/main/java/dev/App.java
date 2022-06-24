17ab471839676966b1c48ea2b362757bb720de34
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-06-24 16:15:13 +0200

    2e commit - importation import com.github.lalyos.jfiglet.FigletFont;

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,10 @@
 package dev;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import com.github.lalyos.jfiglet.FigletFont;
+
+public class App {
+  public static void main(String[] args) {
+    String asciiArt = FigletFont.convertOneLine("hello");
+    System.out.println(asciiArt);
+  }
 }
