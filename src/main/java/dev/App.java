68dbd75a8ebd4739daa2c62dcb37a7e8845961b3
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-06-24 17:14:51 +0200

    3e commit - TP 2 modification pom.xml

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -4,7 +4,7 @@ import com.github.lalyos.jfiglet.FigletFont;
 
 public class App {
   public static void main(String[] args) {
-    String asciiArt = FigletFont.convertOneLine("hello");
+    String asciiArt = FigletFont.convertOneLine("hello world !");
     System.out.println(asciiArt);
   }
 }
